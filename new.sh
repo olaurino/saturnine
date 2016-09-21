bdf269e2203b73c624bd9377a3bd0458938ffd10
Omar Laurino <olaurino@cfa.harvard.edu>
2016-09-21 13:05:44 -0400

    fix exec bit

--- new.sh
+++ new.sh
@@ -8,5 +8,6 @@ fi
 mkdir -p $1
 echo "#!/bin/bash -ex" > $1/make.sh
 echo "$bindir/reveal.sh slides.md meta.yaml" >> $1/make.sh
+chmod u+x $1/make.sh
 cp $bindir/slides.md.template $1/slides.md
 cp $bindir/meta.yaml.template $1/meta.yaml
